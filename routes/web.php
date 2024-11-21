48f462fce76ffb0e245844cf5834cc5485360e56
Retar <markiancuaco40@gmail.com>
2024-11-21 13:25:36 +0800

    storing data

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\accountsCLR;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[accountsCLR::class,'index'])->name('accounts.index');
+Route::get('/AddingItem',[accountsCLR::class,'create'])->name('accounts.create');
+Route::post('/ItemAdded',[accountsCLR::class,'store'])->name('accounts.store');
