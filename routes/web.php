2f9d5aef673316785b9f8e8be605c1c64e674be6
Retar <markiancuaco40@gmail.com>
2024-11-21 16:41:04 +0800

    functionality

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[accountsCLR::class,'index'])->name('accounts.index');
 Route::get('/AddingItem',[accountsCLR::class,'create'])->name('accounts.create');
 Route::post('/ItemAdded',[accountsCLR::class,'store'])->name('accounts.store');
+Route::get('/viewlist',[accountsCLR::class,'view'])->name('accounts.view');
+Route::get('/{id}/show',[accountsCLR::class,'show'])->name('accounts.show');
+Route::get('/{id}/edit',[accountsCLR::class,'edit'])->name('accounts.edit');
+Route::put('/aptapt/{id}',[accountsCLR::class,'update'])->name('accounts.update');
